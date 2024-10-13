5e0c431914c658a456dbf9e7733a35c4c5da2f63
rajivbagade-ws <117102297+rajivbagade-ws@users.noreply.github.com>
2024-10-13 20:03:19 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        IMDb (an initialism for Internet Movie Database)[2] is an online
+        database of information related to films, television series, podcasts,
+        home videos, video games, and streaming content online – including cast,
+        production crew and personal biographies, plot summaries, trivia,
+        ratings, and fan and critical reviews. IMDb began as a fan-operated
+        movie database on the Usenet group "rec.arts.movies" in 1990, and moved
+        to the Web in 1993. Since 1998, it has been owned and operated by
+        IMDb.com, Inc., a subsidiary of Amazon.
+      </p>
+      <p>
+        The site's message boards were disabled in February 2017. As of 2019,
+        IMDb was the 52nd most visited website on the Internet, as ranked by
+        Alexa.[3] As of March 2022, the database contained some 10.1 million
+        titles (including television episodes), 11.5 million person records, and
+        83 million registered users.[4]
+      </p>
+      <p>
+        The title and talent pages of IMDb are accessible to all users, but only
+        registered and logged-in users can submit new material and suggest edits
+        to existing entries. Most of the site's data has been provided by these
+        volunteers. Registered users with a proven track record are able to add
+        and make corrections to cast lists, credits, and some other data points.
+        However, the addition and removal of images, and alterations to titles,
+        cast and crew names, character names, and plot summaries are subject to
+        an approval process; this usually takes between 24 and 72 hours.
+      </p>
+    </div>
+  );
+}
